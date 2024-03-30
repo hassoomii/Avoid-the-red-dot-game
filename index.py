39dd1d43df63c229b7fd352ca3811da15b2a3fa0
hassoomii <120605011+hassoomii@users.noreply.github.com>
2024-03-29 20:51:29 -0500

    First couple edits

--- index.py
+++ index.py
@@ -1,18 +1,26 @@
 import pygame
 import sys
 
+
+class Screen:
+    def __init__(self, width, height, x, y):
+        self.screen_width = width
+        self.screen_height = height
+        self.screen_x = x
+        self.screen_y = y
+
+
+screen_width = 900
+screen_height = screen_width * .75
 pygame.init()
 pygame.font.init()
-pygame.display.set_mode((800, 900))
+pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("WOOOOOOOO")
 clock = pygame.time.Clock()
 
-class Screen:
-    def __init__(self):
-        self.screen_width = 8700
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-
